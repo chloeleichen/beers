8ddf9167f488498e45d0896de60bd43557ccd5cb
Chloe <chloeleichen@gmail.com>
2015-09-11 22:08:48 +1000

    postcode lookup

--- app/view1/view1Controller.js
+++ app/view1/view1Controller.js
@@ -1,11 +1,22 @@
 'use strict';
  module.exports = function($scope, $http) {
+  $scope.distance = "distance from me";
   $http.get('./../data.json')
   .then(function(response){
-    $scope.pubs = response.data;
-    $scope.search;
-    console.log($scope.pubs);
+    init(response);
   }, function(error){
     console.log(error);
   });
+
+  function getDistance(postcode){
+    $scope.distance = postcode;
+  }
+
+  function init(response){
+    $scope.pubs = response.data;
+    $scope.search;
+    $scope.getDistance = getDistance;
+    console.log($scope.pubs);
+
+  }
 };
